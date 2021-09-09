54904c51b31aa88a0a893b09c11e362966191204
mercadier <mercadier86@gmail.com>
2021-09-09 15:19:42 +0100

    ...
    ...

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,50 @@
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static MainManager instance { get; private set; }
+    public Color TeamColor;
+
+    private void Awake()
+    {
+        if(instance!=null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadColor();
+    }
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        //TeamColor = data.TeamColor;
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath+"/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if(File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            TeamColor = data.TeamColor; 
+        }
+    }
+}
